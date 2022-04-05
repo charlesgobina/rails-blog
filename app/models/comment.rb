3e74700f8deda5bc40af7f1bc8e68d9dd696ef5c
charlesgobina <charles.gobina@gmail.com>
2022-04-05 18:21:33 +0100

    Implement comment class and methods

---
+++ app/models/comment.rb
@@ -0,0 +1,12 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+
+
+  after_save :update_posts_comments_counter
+
+  def update_posts_comments_counter
+    post.increment!(:comments_counter)
+  end
+
+end
