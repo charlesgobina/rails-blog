eae88266ca48ff88bada1843176ebc3ddbc72f20
charlesgobina <charles.gobina@gmail.com>
2022-04-05 21:47:05 +0100

    Implement changes requested by reviewer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comment, class_name: 'Comment', foreign_key: 'author_id', dependent: :destroy
-  has_many :like, class_name: 'Like', foreign_key: 'author_id', dependent: :destroy
-
-  after_save :update_post_counter
-
-  def update_post_counter
-    author.increment!(:posts_counter)
-  end
+  belongs_to :author, class_name: 'User'
+  has_many :comment, class_name: 'Comment', dependent: :destroy
+  has_many :like, class_name: 'Like', dependent: :destroy
 
   def self.last_5_comments(id)
     Comment.all.where("post_id = #{id}").order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 end
