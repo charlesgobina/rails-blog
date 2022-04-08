bb01c02f557e53444745e58cf15390af5387a99f
charlesgobina <charles.gobina@gmail.com>
2022-04-08 15:38:48 +0100

    Create new post for current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,32 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @user_posts = User.last_3_posts(params[:user_id])
-    @posts = @user.post
+    @posts = @user.posts
   end
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comment
+    @comments = @post.comments
     @user = @post.author
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = current_user.posts.new(post_params)
+
+    if post.save
+      redirect_to user_posts_path(post.author)
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
