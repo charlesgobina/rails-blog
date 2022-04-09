68e40979e3cc7d1f34890b97776c6dfcd1843978
charlesgobina <charles.gobina@gmail.com>
2022-04-09 01:26:16 +0100

    Add comments and likes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,12 @@
+class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.new(author_id: current_user.id, post_id: @post.id)
+
+    if @like.save
+      redirect_to user_post_path(user_id: current_user.id, id: @post.id), notice: "You liked this post!"
+    else
+      redirect_to user_post_path(user_id: current_user.id, id: @post.id), alert: "Error liking post!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'comments/create'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create]
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
