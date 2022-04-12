e9739e67b66ccce6fd5b0ff0ba5fcd17d0f9c7fc
charlesgobina <charles.gobina@gmail.com>
2022-04-12 14:27:30 +0100

    Configure devise gem for blog app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,22 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: '',
+    password: '',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+ 
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
