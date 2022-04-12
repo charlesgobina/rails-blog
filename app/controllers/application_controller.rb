ea2fcf32db16aee48535416db31eb26800bb3adf
charlesgobina <charles.gobina@gmail.com>
2022-04-12 15:30:40 +0100

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   add_flash_types :info, :error, :warning
 
-  before_action :authenticate_user! 
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
   end
 end
